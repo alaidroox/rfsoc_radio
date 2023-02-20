6a099d5c182d651588d60c2dbb65a8293dc32a27
Salem AlAidroos <alaidroox@gmail.com>
2023-02-20 14:45:04 +0100

    Second Commit

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ from setuptools import find_packages, setup
 
 # global variables
 package_name = 'rfsoc_radio'
-pip_name = 'rfsoc-radio'
-board = os.environ['BOARD']
-repo_board_folder = f'boards/{board}/{package_name}'
+#pip_name = 'rfsoc-radio'
+#board = os.environ['BOARD']
+repo_board_folder = f'vivado'
 board_notebooks_dir = os.environ['PYNQ_JUPYTER_NOTEBOOKS']
 board_project_dir = os.path.join(board_notebooks_dir, 'rfsoc-studio', 'radio-demonstrator')
 
 data_files = []
 
+'''
 # check whether board is supported
 def check_env():
     if not os.path.isdir(repo_board_folder):
@@ -21,6 +22,7 @@ def check_env():
     if not os.path.isdir(board_notebooks_dir):
         raise ValueError(
             "Directory {} does not exist.".format(board_notebooks_dir))
+'''
 
 # copy overlays to python package
 def copy_overlays():
@@ -32,22 +34,25 @@ def copy_overlays():
 
 # copy notebooks to jupyter home
 def copy_notebooks():
-    src_nb_dir = os.path.join(repo_board_folder, 'notebooks')
+    src_nb_dir = os.path.join('notebooks')
     dst_nb_dir = os.path.join(board_project_dir)
     if os.path.exists(dst_nb_dir):
         shutil.rmtree(dst_nb_dir)
     copy_tree(src_nb_dir, dst_nb_dir)
 
 def copy_common_images():
-    src_cm_dir = os.path.join(f'boards/common/', 'images')
+    src_cm_dir = os.path.join(f'notebooks', 'images')
     dst_cm_dir = os.path.join(board_project_dir, 'images')
     copy_tree(src_cm_dir, dst_cm_dir)
 
+'''
 def copy_common_notebooks():
     src_cm_dir = os.path.join(f'boards/common/', 'notebooks')
     dst_cm_dir = os.path.join(board_project_dir)
     copy_tree(src_cm_dir, dst_cm_dir)
+'''
 
+'''
 # copy xrfclk file to python package (gen3 devices only)
 def copy_xrfclk():
     src_at_dir = os.path.join(repo_board_folder, 'xrfclk')
@@ -56,13 +61,14 @@ def copy_xrfclk():
         copy_tree(src_at_dir, dst_at_dir)
         data_files.extend(
             [os.path.join("..", dst_at_dir, f) for f in os.listdir(dst_at_dir)])
+'''
 
-check_env()
+#check_env()
 copy_overlays()
 copy_notebooks()
 copy_common_images()
-copy_common_notebooks()
-copy_xrfclk()
+#copy_common_notebooks()
+#copy_xrfclk()
 
 setup(
     name="rfsoc_radio",
